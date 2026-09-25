f0340d85af127569ecb047b3bc2aa7bac0dc6f4b
Your Name <rayronvidal81@gmail.com>
2026-09-25 15:24:08 -0300

    Prisma adicionado

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, env} from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+
+  datasource: {
+    url: "mysql://root:@localhost:3306/orbi",
+  },
+});
